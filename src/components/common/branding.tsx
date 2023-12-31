1bc51ed415956474636975ff0c184969a24da703
s4shibam <shibamsaha2002@gmail.com>
2024-01-01 03:28:29 +0530

    fix: modify logo animation

--- src/components/common/branding.tsx
+++ src/components/common/branding.tsx
@@ -1,13 +1,19 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
-import LOGO from '@assets/animated-logo.svg'
+import LOGO from '@assets/logo-animated.svg'
 
 const Branding = () => {
   return (
     <Link className="flex items-center gap-1" href="/">
-      <Image alt="" height={50} src={LOGO} width={50} />
-      <p className="text-2xl font-semibold tracking-wide">Ckret</p>
+      <Image
+        alt=""
+        className="-ml-1 aspect-square h-[40px] w-[40px] sm:ml-0 sm:h-[50px] sm:w-[50px]"
+        height={50}
+        src={LOGO}
+        width={50}
+      />
+      <p className="text-[25px] font-bold tracking-wider sm:text-3xl">Ckret.</p>
     </Link>
   )
 }
