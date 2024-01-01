644beeb1105f2a142fa9d502c4cfc8ca770d1860
s4shibam <shibamsaha2002@gmail.com>
2024-01-02 01:27:07 +0530

    feat: google sign in flow

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,36 @@
-import { NextAuthOptions } from 'next-auth';
-
+import { NextAuthOptions } from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
 
 export const authOptions: NextAuthOptions = {
-    secret: process.env.NEXTAUTH_SECRET,
-    providers: []
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      authorization: {
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code'
+        }
+      }
+    })
+  ],
+  callbacks: {
+    async signIn({ user, profile, ...rest }) {
+      console.log(user, profile, rest)
+      return true
+    },
+    session({ session }) {
+      console.log(session)
+      return session
+    },
+    redirect({ url, baseUrl }) {
+      if (url.startsWith('/')) {
+        return `${baseUrl}${url}`
+      } else {
+        return `${baseUrl}/dashboard/profile`
+      }
+    }
   }
+}
