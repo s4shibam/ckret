598578e4aa1a1be623dfa8e0083d495a16e71edd
s4shibam <shibamsaha2002@gmail.com>
2024-01-04 05:28:43 +0530

    feat: add axios interceptors

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { getSession } from 'next-auth/react'
 const ckretConnectURL = process.env.NEXT_PUBLIC_CKRET_CONNECT_URL
 
 // Custom Axios instance with common configurations
@@ -8,5 +9,34 @@ const ckretConnect = axios.create({
     'Content-Type': 'application/json'
   }
 })
+
+// Interceptors
+ckretConnect.interceptors.request.use(
+  async (config) => {
+    config.headers = config.headers || {}
+    const session = await getSession()
+
+    if (session?.user?.token && !config.headers.Authorization) {
+      const authToken = `Bearer ${session.user.token}`
+      config.headers.Authorization = authToken
+    }
+    return config
+  },
+  (error) => {
+    console.error('Failed to send request: ', error)
+    return Promise.reject(error)
+  }
+)
+
+ckretConnect.interceptors.response.use(
+  (response) => {
+    return response.data
+  },
+  (error) => {
+    console.error('API Error: ', error)
+
+    return Promise.reject(error?.response?.data)
+  }
+)
 
 export default ckretConnect
