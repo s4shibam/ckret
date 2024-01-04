02b2b25f612d902655fec65183c99ddc14982b45
s4shibam <shibamsaha2002@gmail.com>
2024-01-04 17:13:57 +0530

    feat: add fn to validate text length

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const isInvalidLength = (
+  text: string,
+  LIMIT: { MAX: number; MIN: number }
+) => {
+  return text.length < LIMIT.MIN || text.length > LIMIT.MAX
 }
