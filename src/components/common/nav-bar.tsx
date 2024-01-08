b0431500c851c5be727c01463c9a8183f5947b19
s4shibam <shibamsaha2002@gmail.com>
2024-01-09 05:04:07 +0530

    feat: add bg transparent to opaque animation

--- src/components/common/nav-bar.tsx
+++ src/components/common/nav-bar.tsx
@@ -3,6 +3,7 @@
 import { LayoutDashboard, LogIn } from 'lucide-react'
 import Link from 'next/link'
 import { useSession } from 'next-auth/react'
+import { useEffect, useState } from 'react'
 
 import { Button } from '@components/ui/button'
 
@@ -11,9 +12,32 @@ import SignInModal from './signin-modal'
 
 const NavBar = () => {
   const session = useSession()
+  const [scrolled, setScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 75) {
+        setScrolled(true)
+      } else {
+        setScrolled(false)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
   return (
-    <div className="fixed inset-x-0 top-0 z-10 h-16 border-b bg-white shadow-sm">
-      <div className="mx-auto flex h-full w-full max-w-7xl items-center justify-between px-5">
+    <div className="fixed inset-x-0 top-0 z-10 h-16 bg-transparent">
+      <div
+        className={`fixed inset-x-0 top-0 border-b bg-white shadow-sm transition-all duration-500 ease-in-out ${
+          scrolled ? 'h-16' : 'h-0'
+        }`}
+      />
+      <div className="relative mx-auto flex h-full w-full max-w-7xl items-center justify-between px-5">
         <Branding />
         {(session.status === 'loading' ||
           session.status === 'unauthenticated') && (
