561ddf80963f2d213536165709cca91b5d69d5ce
s4shibam <shibamsaha2002@gmail.com>
2024-01-09 05:04:56 +0530

    style: revise background image

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -4,31 +4,29 @@ import HERO from '@assets/hero.webp'
 
 const Hero = () => {
   return (
-    <div className="graph-paper relative flex h-fit w-full flex-col items-center justify-center gap-10 overflow-hidden border-b-2 border-ckret-primary px-6">
-      <div className="absolute top-1/3 h-[80vh] w-12 rotate-[15deg] bg-ckret-secondary blur-[100px] xl:w-16" />
-      <div className="flex flex-col gap-4 pt-[10vh] drop-shadow-lg">
-        <p className="animate-fade-up overflow-hidden text-center text-6xl font-bold uppercase xl:text-8xl">
+    <div className="doodle relative flex min-h-screen w-full flex-col items-center justify-center gap-10 overflow-hidden border-b-2 border-ckret-primary px-6 pt-16">
+      <div className="flex select-none flex-col gap-4 pt-20 tracking-widest drop-shadow-xl">
+        <p className="animate-fade-up overflow-hidden text-center text-6xl font-bold uppercase sm:text-7xl lg:text-8xl">
           Speak Your{' '}
-          <span className="bg-gradient-to-br from-orange-500 to-rose-500 bg-clip-text tracking-wider text-transparent">
+          <span className="bg-gradient-to-br from-orange-500 to-rose-500 bg-clip-text text-transparent">
             Mind
           </span>
         </p>
-        <p className="animate-fade-up overflow-hidden text-center text-6xl font-bold uppercase xl:text-8xl">
+        <p className="animate-fade-up overflow-hidden text-center text-6xl font-bold uppercase sm:text-7xl lg:text-8xl">
           Keep Your{' '}
-          <span className="bg-gradient-to-br from-lime-500 to-teal-500 bg-clip-text tracking-wider text-transparent">
+          <span className="bg-gradient-to-br from-lime-500 to-teal-500 bg-clip-text text-transparent">
             Identity
           </span>
         </p>
       </div>
-      <div className="absolute inset-x-0 top-0 h-10 bg-gradient-to-b from-white to-transparent" />
-      <div className="absolute inset-x-0 bottom-0 h-10 bg-gradient-to-t from-white to-transparent" />
+      <div className="absolute inset-x-0 top-0 h-16 bg-gradient-to-b from-white to-transparent" />
       <Image
         alt="Image by Freepik"
         // eslint-disable-next-line tailwindcss/classnames-order
-        className="relative animate-fade-up drop-shadow-lg"
-        height={500}
+        className="relative mt-auto animate-fade-up drop-shadow-lg"
+        height={550}
         src={HERO}
-        width={500}
+        width={550}
       />
     </div>
   )
