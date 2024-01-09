85a0ab9969f938e0c56e2a0ddb06e80e008b7b6c
s4shibam <shibamsaha2002@gmail.com>
2024-01-10 00:42:06 +0530

    fix: fix metadata base path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,31 @@
 import type { Metadata, Viewport } from 'next'
 import { Fredoka } from 'next/font/google'
 
+import OG_IMAGE from '@assets/og-image.png'
+
 import './globals.css'
 import AppProviders from '@providers/app-providers'
 
-import { META_DESCRIPTION, META_KEYWORDS, META_TITLE } from '@lib/constants'
+import {
+  CKRET_URL,
+  META_DESCRIPTION,
+  META_KEYWORDS,
+  META_TITLE
+} from '@lib/constants'
 
 import GoogleAnalytics from '@components/analytics/google-analytics'
 
 const fredoka = Fredoka({ subsets: ['latin'] })
+
+const imagesArray = [
+  {
+    url: OG_IMAGE.src,
+    width: OG_IMAGE.width,
+    height: OG_IMAGE.height,
+    alt: 'Ckret'
+  },
+  './icon.ico'
+]
 
 export const metadata: Metadata = {
   manifest: 'manifest.json',
@@ -16,22 +33,15 @@ export const metadata: Metadata = {
   description: META_DESCRIPTION,
   keywords: META_KEYWORDS,
   openGraph: {
-    images: ['../assets/og-image.png', './icon.ico']
+    images: imagesArray
   },
   twitter: {
     card: 'summary_large_image',
     title: META_TITLE,
     description: META_DESCRIPTION,
-    images: [
-      {
-        url: '../assets/og-image.png',
-        width: 1200,
-        height: 630,
-        alt: 'Ckret'
-      },
-      './icon.ico'
-    ]
-  }
+    images: imagesArray
+  },
+  metadataBase: new URL(CKRET_URL)
 }
 
 export const viewport: Viewport = {
