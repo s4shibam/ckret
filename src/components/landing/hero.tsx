a2f17a51e686b70dd5ea410b4841fc03c717218b
s4shibam <shibamsaha2002@gmail.com>
2024-12-06 11:09:18 +0530

    style: resolve height issue

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -4,8 +4,8 @@ import HERO from '@assets/hero.webp'
 
 const Hero = () => {
   return (
-    <div className="doodle relative flex min-h-screen w-full flex-col items-center justify-center gap-10 overflow-hidden border-b-2 border-ckret-primary px-6 pt-16">
-      <div className="flex select-none flex-col gap-4 pt-20 tracking-widest drop-shadow-xl">
+    <div className="doodle relative flex size-full min-h-screen flex-col items-center justify-between gap-10 overflow-hidden border-b-2 border-ckret-primary px-6 pt-28">
+      <div className="flex select-none flex-col gap-4 tracking-widest drop-shadow-xl">
         <p className="animate-fade-up overflow-hidden text-center text-6xl font-bold uppercase sm:text-7xl lg:text-8xl">
           Speak Your{' '}
           <span className="bg-gradient-to-br from-orange-500 to-rose-500 bg-clip-text text-transparent">
@@ -22,11 +22,10 @@ const Hero = () => {
       <div className="absolute inset-x-0 top-0 h-16 bg-gradient-to-b from-white to-transparent" />
       <Image
         alt="Image by Freepik"
-        // eslint-disable-next-line tailwindcss/classnames-order
-        className="relative mt-auto animate-fade-up drop-shadow-lg"
-        height={550}
+        className="relative size-[350px] animate-fade-up drop-shadow-lg sm:size-[400px] lg:size-[450px]"
+        height={450}
         src={HERO}
-        width={550}
+        width={450}
       />
     </div>
   )
