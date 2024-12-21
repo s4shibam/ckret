8c96c37389a1cfb825b003a47cee7acedf19d751
s4shibam <shibamsaha2002@gmail.com>
2024-12-21 16:02:37 +0530

    style: implement responsiveness + reduce redundancy

--- src/components/dashboard/header.tsx
+++ src/components/dashboard/header.tsx
@@ -7,9 +7,11 @@ interface HeaderProps {
 
 const Header = ({ title, children }: HeaderProps) => {
   return (
-    <nav className="fixed inset-x-0 top-0 h-16 border-b border-gray-300 bg-white md:pl-[18rem]">
+    <nav className="fixed inset-x-0 top-0 z-30 h-16 border-b border-gray-300 bg-white md:pl-[18rem]">
       <div className="flex h-full items-center justify-between gap-4 p-4 md:px-8">
-        <p className="heading">{title}</p>
+        <p className="text-xl font-medium capitalize tracking-wider sm:text-2xl">
+          {title}
+        </p>
         {children}
       </div>
     </nav>
