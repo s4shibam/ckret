05845dc9cea50e11824653a23d267b4523d1747c
s4shibam <shibamsaha2002@gmail.com>
2024-12-21 16:40:16 +0530

    feat: add message and sketch layouts with metadata generation

---
+++ src/app/cl/[username]/sketch/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from 'next'
+import { unstable_noStore as noStore } from 'next/cache'
+
+type Props = {
+  params: { username: string }
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const username = params.username
+  noStore()
+  const data = await fetch(
+    `${process.env.NEXT_PUBLIC_CKRET_CONNECT_URL}/user/details/${username}`
+  ).then((res) => res.json())
+
+  const metadata = {
+    title: `Ckret - ${data?.data?.name || 'User not found'}`,
+    description: 'Send me anonymous sketches.'
+  }
+
+  return metadata
+}
+
+export default function SketchLayout({
+  children
+}: {
+  children: React.ReactNode
+}) {
+  return <>{children}</>
+}
