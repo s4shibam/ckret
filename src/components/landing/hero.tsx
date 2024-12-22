d0134e1eaf6774c77ff8cd7602ed4a7d56036c38
s4shibam <shibamsaha2002@gmail.com>
2024-12-23 05:15:58 +0530

    feat: add new hero image and enhance hero component layout

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -1,32 +1,58 @@
 import Image from 'next/image'
 
-import HERO from '@assets/hero.webp'
+import HERO from '@assets/ckret-hero-image.webp'
+import CreateLink from '@components/common/create-link'
 
 const Hero = () => {
   return (
-    <div className="doodle relative flex size-full min-h-screen flex-col items-center gap-10 overflow-hidden border-b-2 border-ckret-primary px-6 pt-28">
-      <div className="mt-auto flex select-none flex-col gap-4 tracking-widest drop-shadow-xl">
-        <p className="animate-fade-up overflow-hidden text-center text-5xl font-extrabold uppercase sm:text-7xl lg:text-8xl">
-          Speak Your{' '}
-          <span className="bg-gradient-to-br from-orange-500 to-rose-500 bg-clip-text text-transparent">
-            Mind
-          </span>
-        </p>
-        <p className="animate-fade-up overflow-hidden text-center text-5xl font-extrabold uppercase sm:text-7xl lg:text-8xl">
-          Keep Your{' '}
-          <span className="bg-gradient-to-br from-lime-500 to-teal-500 bg-clip-text text-transparent">
-            Identity
-          </span>
-        </p>
+    <div className="doodle relative flex size-full min-h-screen flex-col items-center justify-center gap-10 border-b-2 border-ckret-primary px-6 pb-10 pt-20 sm:pt-24 md:flex-row md:pt-10">
+      <div className="space-y-6">
+        <div className="flex select-none flex-col justify-center text-4xl drop-shadow-xl sm:text-6xl md:justify-start md:text-7xl">
+          <p className="animate-fade-up overflow-hidden text-center font-extrabold md:text-left">
+            Speak Your{' '}
+            <span className="bg-gradient-to-br from-ckret-primary to-ckret-secondary bg-clip-text text-transparent">
+              Mind.
+            </span>
+          </p>
+          <p className="animate-fade-up overflow-hidden text-center font-extrabold md:text-left">
+            Keep Your{' '}
+            <span className="bg-gradient-to-tr from-ckret-primary to-ckret-secondary bg-clip-text text-transparent">
+              Identity!
+            </span>
+          </p>
+        </div>
+
+        <div className="hidden max-w-md animate-fade-up rounded-2xl bg-gradient-to-br from-zinc-600 to-zinc-800 p-4 md:block">
+          <div className="mx-auto w-full max-w-md">
+            <CreateLink />
+          </div>
+        </div>
       </div>
+
       <div className="absolute inset-x-0 top-0 h-16 bg-gradient-to-b from-white to-transparent" />
-      <Image
-        alt="Image by Freepik"
-        className="relative size-[350px] animate-fade-up drop-shadow-lg sm:size-[400px] lg:size-[450px]"
-        height={450}
-        src={HERO}
-        width={450}
-      />
+
+      <div className="group relative h-auto w-[28rem] animate-fade-up overflow-hidden rounded-2xl drop-shadow-lg md:w-[32rem]">
+        <Image
+          draggable={false}
+          alt="Ckret Hero Image"
+          className="size-full transition-all duration-500 hover:scale-105"
+          height={1500}
+          src={HERO}
+          width={1500}
+        />
+
+        <div className="absolute inset-x-0 bottom-0 bg-gradient-to-b from-transparent to-ckret-secondary/20 px-2 py-4 text-center text-lg font-medium tracking-wide text-black opacity-0 transition-all duration-500 group-hover:opacity-100 md:text-xl">
+          Santa's Profile on Ckret.
+        </div>
+
+        <div className="absolute bottom-0 left-5 right-5 h-px bg-gradient-to-r from-transparent via-ckret-secondary to-transparent" />
+      </div>
+
+      <div className="w-full max-w-md animate-fade-up rounded-2xl bg-gradient-to-br from-zinc-600 to-zinc-800 p-4 md:hidden">
+        <div className="mx-auto w-full max-w-md">
+          <CreateLink />
+        </div>
+      </div>
     </div>
   )
 }
