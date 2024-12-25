0663eeff6fff2079b624a5b158f7e6a0803e5ca3
s4shibam <shibamsaha2002@gmail.com>
2024-12-25 18:07:19 +0530

    chore: re-configure eslint, prettier + add commit linting with husky and lint-staged + improve ide settings

---
+++ commitlint.config.ts
@@ -0,0 +1,35 @@
+import type { UserConfig } from '@commitlint/types'
+
+const configuration: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'chore',
+        'revert',
+        'build',
+        'ci'
+      ]
+    ],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'subject-case': [
+      2,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case']
+    ]
+  }
+}
+
+export default configuration
